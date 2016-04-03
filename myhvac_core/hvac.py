0d3463d53526b16ca13497b7e0299329843a3506
Alan Quillin <alanquillin@gmail.com>
2016-04-02 20:52:03 -0400

    Added set_system_state to hvac module

--- myhvac_core/hvac.py
+++ myhvac_core/hvac.py
@@ -1,36 +1,45 @@
 import cfg
 import logging
 import RPi.GPIO as GPIO
-import system_state as state
+import threading
+
+from myhvac_core import system_state as state
 
 LOG = logging.getLogger(__name__)
 
 io_opts = [
-    cfg.IntOpt('green_pin_out', required=True, help='GPIO BCM pin output number for green (FAN) wire'),
-    cfg.IntOpt('green_pin_in', required=True, help='GPIO BCM pin input number for green (FAN) wire'),
-    cfg.IntOpt('white_pin_out', required=True, help='GPIO BCM pin output number for white (HEAT) wire'),
-    cfg.IntOpt('white_pin_in', required=True, help='GPIO BCM pin input number for white (HEAT) wire'),
-    cfg.IntOpt('yellow_pin_out', required=True, help='GPIO BCM pin output number for yellow (COOL) wire'),
-    cfg.IntOpt('yellow_pin_in', required=True, help='GPIO BCM pin input number for yellow (COOL) wire')
+    cfg.IntOpt('green_pin_out', required=True, help='GPIO pin output number for green (FAN) wire'),
+    cfg.IntOpt('green_pin_fb', required=True, help='GPIO pin feedback number for green (FAN) wire'),
+    cfg.IntOpt('green_pin_lb', required=True, help='GPIO pin loopback number for green (FAN) wire'),
+    cfg.IntOpt('white_pin_out', required=True, help='GPIO pin output number for white (HEAT) wire'),
+    cfg.IntOpt('white_pin_fb', required=True, help='GPIO pin feedback number for white (HEAT) wire'),
+    cfg.IntOpt('white_pin_lb', required=True, help='GPIO pin loopback number for white (HEAT) wire'),
+    cfg.IntOpt('yellow_pin_out', required=True, help='GPIO pin output number for yellow (COOL) wire'),
+    cfg.IntOpt('yellow_pin_fb', required=True, help='GPIO pin feedback number for yellow (COOL) wire'),
+    cfg.IntOpt('yellow_pin_lb', required=True, help='GPIO pin loopback number for yellow (COOL) wire'),
+    cfg.StrOpt('pin_mode', default='board', help='GPIO pin mode.  Available values: [board, bcm]')
 ]
-
-cfg.CONF.register_opts(io_opts, 'io')
+CONF = cfg.CONF
+CONF.register_opts(io_opts, 'io')
 
 
 def init_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(cfg.CONF.io.green_pin_out, GPIO.OUT)
-    GPIO.setup(cfg.CONF.io.green_pin_in, GPIO.IN)
-    GPIO.setup(cfg.CONF.io.white_pin_out, GPIO.OUT)
-    GPIO.setup(cfg.CONF.io.white_pin_in, GPIO.IN)
-    GPIO.setup(cfg.CONF.io.yellow_pin_out, GPIO.OUT)
-    GPIO.setup(cfg.CONF.io.yellow_pin_in, GPIO.IN)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(CONF.io.green_pin_out, GPIO.OUT)
+    GPIO.setup(CONF.io.green_pin_fb, GPIO.IN)
+    GPIO.setup(CONF.io.green_pin_lb, GPIO.IN)
+    GPIO.setup(CONF.io.white_pin_out, GPIO.OUT)
+    GPIO.setup(CONF.io.white_pin_fb, GPIO.IN)
+    GPIO.setup(CONF.io.white_pin_lb, GPIO.IN)
+    GPIO.setup(CONF.io.yellow_pin_out, GPIO.OUT)
+    GPIO.setup(CONF.io.yellow_pin_fb, GPIO.IN)
+    GPIO.setup(CONF.io.yellow_pin_lb, GPIO.IN)
 
 
 def get_system_state():
-    is_green_on = GPIO.input(cfg.CONF.io.green_pin_in)
-    is_white_on = GPIO.input(cfg.CONF.io.green_pin_in)
-    is_yellow_on = GPIO.input(cfg.CONF.io.green_pin_in)
+    is_green_on = GPIO.input(CONF.io.green_pin_in)
+    is_white_on = GPIO.input(CONF.io.green_pin_in)
+    is_yellow_on = GPIO.input(CONF.io.green_pin_in)
 
     if is_green_on and is_white_on and is_yellow_on:
         return state.COOL
@@ -44,19 +53,52 @@ def get_system_state():
     return state.UNKNOWN
 
 
-def heat_on():
-    GPIO.output(cfg.CONF.io.green_pin_out, True)
-    GPIO.output(cfg.CONF.io.white_pin_out, True)
-    GPIO.output(cfg.CONF.io.yellow_pin_out, False)
+def set_system_state(to_state, current_state):
+    if to_state == state.UNKNOWN:
+        return
 
+    def _set_system_state(s):
+        LOG.info('Setting system: %s', state.print_state(s))
+        if s == state.HEAT:
+            _heat_on()
+        elif s == state.COOL:
+            _cool_on()
+        elif s == state.FAN_ONLY:
+            _fan_only()
+        elif s == state.OFF:
+            _off()
 
-def cool_on():
-    GPIO.output(cfg.CONF.io.green_pin_out, True)
-    GPIO.output(cfg.CONF.io.white_pin_out, True)
-    GPIO.output(cfg.CONF.io.yellow_pin_out, True)
+        # Verify that the system state was set currently
+        threading._sleep(2)
+        cs = get_system_state()
+        if s != cs:
+            LOG.error('The system state was not set correctly!  Expected state: %s, Actual state: %s',
+                      state.print_state(s), state.print_state(cs))
 
+    def _heat_on():
+        GPIO.output(CONF.io.green_pin_out, True)
+        GPIO.output(CONF.io.white_pin_out, True)
+        GPIO.output(CONF.io.yellow_pin_out, False)
 
-def fan_only():
-    GPIO.output(cfg.CONF.io.green_pin_out, True)
-    GPIO.output(cfg.CONF.io.white_pin_out, False)
-    GPIO.output(cfg.CONF.io.yellow_pin_out, False)
+    def _cool_on():
+        GPIO.output(CONF.io.green_pin_out, True)
+        GPIO.output(CONF.io.white_pin_out, True)
+        GPIO.output(CONF.io.yellow_pin_out, True)
+
+    def _fan_only():
+        GPIO.output(CONF.io.green_pin_out, True)
+        GPIO.output(CONF.io.white_pin_out, False)
+        GPIO.output(CONF.io.yellow_pin_out, False)
+
+    def _off():
+        GPIO.output(CONF.io.green_pin_out, False)
+        GPIO.output(CONF.io.white_pin_out, False)
+        GPIO.output(CONF.io.yellow_pin_out, False)
+
+    if current_state in [state.OFF, state.FAN_ONLY]:
+        _set_system_state(to_state)
+    else:
+        # First set the system to fan only to let the system cool down for 2 minutes, then set the system state
+        _set_system_state(state.FAN_ONLY)
+        threading._sleep(120)
+        _set_system_state(to_state)
--- myhvac_core/system_state.py
+++ myhvac_core/system_state.py
@@ -1,4 +1,11 @@
-HEAT = 'heat'
-COOL = 'cool'
-FAN_ONLY = 'fan_only'
-UNKNOWN = 'unknown'
+OFF = 0
+HEAT = 1
+COOL = 2
+FAN_ONLY = 3
+UNKNOWN = -1
+
+
+def print_state(state):
+    states = {HEAT: "Heat", COOL: "Cool", FAN_ONLY: 'Fan Only', UNKNOWN: 'Unknown'}
+
+    return state.get(state, states[UNKNOWN])
